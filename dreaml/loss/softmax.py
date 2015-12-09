updated kitchen sinks to properly keep the row labels. fixed PCA test to account for factors of -1. updated softmax to work with the new form of SGD

dreaml/loss/softmax.py
@@ -4,19 +4,30 @@ import numpy as np
 class Softmax(Objective):
     
     @staticmethod
-    def f(theta_df,X_df,y_df,reg=0.01,fval=True,grad=True):
+    def f(theta_df,X_df,y_df,reg=0.01):
+        theta = theta_df.get_matrix()
+        penalty = reg/2*np.power(theta,2).sum()
+        # X = X_df.get_matrix(readonly=True)
+        # y = y_df.get_matrix(readonly=True)
+        # P = Softmax._multiclass_prob(theta,X)
+        # n = X.shape[0]
+
+        # cost = np.mean(np.log([ P[y[i,0],i] for i in np.arange(n)]))
+        cost = np.mean(Softmax.f_vec(theta_df,X_df,y_df,reg=reg))
+        return -cost+penalty
+    
+    @staticmethod
+    def f_vec(theta_df,X_df,y_df,reg=0.01):
         theta = theta_df.get_matrix()
         X = X_df.get_matrix(readonly=True)
         y = y_df.get_matrix(readonly=True)
         P = Softmax._multiclass_prob(theta,X)
         n = X.shape[0]
 
-        cost = np.mean(np.log([ P[y[i,0],i] for i in np.arange(n)]))
-        penalty = reg/2*np.power(theta,2).sum()
-        return -cost+penalty
-    
+        return np.log([ P[y[i,0],i] for i in np.arange(n)])
+
     @staticmethod
-    def g(theta_df,X_df,y_df,reg=0.01,fval=True,grad=True):
+    def g(theta_df,X_df,y_df,reg=0.01):
         theta = theta_df.get_matrix()
         X = X_df.get_matrix(readonly=True)
         y = y_df.get_matrix(readonly=True)
@@ -33,10 +44,20 @@ class Softmax(Objective):
         return -(total/len(y))+penalty
     
     @staticmethod
-    def structure(X_df,y_df,reg=0.01,fval=True,grad=True):
+    def structure(X_df,y_df,reg=0.01):
         rows = [str(k) for k in np.unique(y_df.get_matrix())]
         cols = X_df._col_index.keys()
         return rows,cols
+
+    @staticmethod
+    def err(theta_df,X_df,y_df,reg=0.01):
+        theta = theta_df.get_matrix()
+        X = X_df.get_matrix(readonly=True)
+        y = y_df.get_matrix(readonly=True)
+        n = X.shape[0]
+        P = Softmax._multiclass_prob(theta,X)
+        y_pred = np.argmax(P,axis=0)
+        return np.equal(np.squeeze(y),y_pred)
 
     @staticmethod
     def _multiclass_prob(theta,X):

dreaml/tests/test_transformations.py
@@ -157,8 +157,8 @@ class TestTransformation:
         # print M1_reconstructed
 
         M1_reconstructed2 = M1.dot(v).dot(v.T)
-        print M1_reconstructed2
-        print M1.dot(v.dot(v.T))
+        # print M1_reconstructed2
+        # print M1.dot(v.dot(v.T))
 
 
         covmat = (1./(n-1))*M1.T.dot(M1)
@@ -179,5 +179,9 @@ class TestTransformation:
         pca_path = ("pca/","pca/")
         df[pca_path] = PCA(df[M1_path],df[M3_path],d)
         pca = df[pca_path].get_matrix()
-        assert np.isclose(pca,M3.dot(evmat_sorted)).all()
+        proj = M3.dot(evmat_sorted)
+
+        for i in range(pca.shape[1]):
+            assert np.isclose(pca[:,i], proj[:,i]).all() or \
+                   np.isclose(pca[:,i],-proj[:,i]).all()
 

dreaml/transformations/kitchensinks.py
@@ -27,4 +27,5 @@ class KitchenSinks(BatchTransform):
         W = random.randn(X.shape[1], num_features) / s / np.sqrt(2)
         B = random.uniform(0, 2*np.pi, num_features)
 
-        return DataFrame.from_matrix(np.cos(X.dot(W)+ B))
+        return DataFrame.from_matrix(np.cos(X.dot(W)+ B),
+                                     row_labels=X_df.rows())