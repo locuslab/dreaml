9a4a9568144aa87753e8a60e32fa6b884cd25afd
Eric Wong <riceric22@gmail.com>
2015-12-10 16:32:11 -0500

    added new kwarg to the server start command to control new window opening on start

--- dreaml/server.py
+++ dreaml/server.py
@@ -3,7 +3,7 @@ from frontend.start import create_app
 from dreaml import DataFrame
 import webbrowser
 
-def start(df):
+def start(df,new=1):
     """Launches a flask webapp to serve data visualizations for the
     dataframe df. By default this launches at http://localhost:5000"""
     if hasattr(df,"web_thread"):
@@ -12,7 +12,8 @@ def start(df):
         f = lambda a: create_app(a).run(debug=True,use_reloader=False)
         df.web_thread = Thread(target=f, args=(df,))
         df.web_thread.start()
-        webbrowser.open("http://localhost:5000")
+        if new>=0:
+            webbrowser.open("http://localhost:5000",new=new)    
 
 def _start_empty():
     df = DataFrame()
