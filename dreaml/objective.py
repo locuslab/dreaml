3c4263079a4179ce84b376dd9097a1dab942aad2
Eric Wong <riceric22@gmail.com>
2016-01-12 15:59:12 -0500

    created a toy objective for testing purposes

--- dreaml/objective.py
+++ dreaml/objective.py
@@ -5,22 +5,40 @@ from random import randint
 from abc import ABCMeta, abstractmethod
 
 from loss import Softmax
+from loss import Square
 
 
 class Objective(object):
     __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def __init__(self, target_df, *args, **kwargs):
+        pass
     
     @abstractmethod
-    def f(target_df,*args,**kwargs):
+    def f(self):
         pass
 
     @abstractmethod
-    def g(target_df,*args,**kwargs):
+    def g(self):
         pass
 
     @abstractmethod
     def structure(*args,**kwargs):
         pass
+
+class SquareTest(Objective):
+    def __init__(self, y_prediction_df, y_target_df):
+        self.loss = Square(y_prediction_df.get_matrix(),y_target_df.get_matrix()) 
+        self.n = y_target_df.shape[0]
+        self.yt_df = y_target_df
+
+    def f(self): return np.mean(self.loss.f())
+    def g(self): return self.loss.g()/self.n
+    def h(self): pass
+
+    @staticmethod
+    def structure(y_target_df): return y_target_df.rows(),y_target_df.cols()
 
 class SoftmaxRegression(Objective):
     def __init__(self,theta_df,X_df,y_df,reg):
@@ -45,5 +63,7 @@ class SoftmaxRegression(Objective):
     def h(self): 
         # Needs to be averaged and also regularized
         return self.loss.h()
-    def structure(self):
-        return self.y_df.cols(), self.X_df.rows()
+
+    @staticmethod
+    def structure(X_df,y_df,reg):
+        return y_df.cols(), X_df.rows()
