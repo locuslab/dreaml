c5634b0d4d690d8a4a2c81021f0598d6962cadaa
Eric <riceric22@gmail.com>
2016-01-22 23:09:14 -0500

    fixed metrics and onehotencoding to work with new loss framework

--- dreaml/transformations/onehotencoding.py
+++ dreaml/transformations/onehotencoding.py
@@ -1,23 +1,23 @@
 from dreaml.dataframe.transform import BatchTransform
 from dreaml.dataframe.dataframe import DataFrame
-from scipy.sparse import lil_matrix,csr_matrix
+# from scipy.sparse import lil_matrix,csr_matrix
+import numpy as np
 
 class OneHotEncoding(BatchTransform):
     def func(self,target_df,Y_df):
-        print "OHE....."
-        Y = Y_df.get_matrix().flatten().tolist()
+        Y = Y_df.get_matrix().squeeze().tolist()
         d = {}
         for y in Y:
             if y not in d:
                 d[y] = len(d)
         for i,y in enumerate(sorted(d.keys())):
-            print i,y
             d[y] = i
-        OHE = lil_matrix((len(Y),len(d)))
+        OHE = np.zeros((len(Y),len(d)),dtype=bool)
         for i,y in enumerate(Y):
-            OHE[i,d[y]] = 1
+            OHE[i,d[y]] = True
         
         row_labels =Y_df._row_index.keys()
         col_labels = [str(i) for i in range(len(d))]
 
-        return DataFrame.from_matrix(OHE.tocsr(),row_labels,col_labels)
+        print OHE.shape
+        target_df.set_matrix(OHE,row_labels,col_labels)
