45bf7d5a4ee368956e3a207021552a242c98dce4
Eric Wong <riceric22@gmail.com>
2016-02-03 21:08:14 -0500

    fixed bug for ohe

--- dreaml/transformations/onehotencoding.py
+++ dreaml/transformations/onehotencoding.py
@@ -1,23 +1,27 @@
 from dreaml.dataframe.transform import BatchTransform
 from dreaml.dataframe.dataframe import DataFrame
-# from scipy.sparse import lil_matrix,csr_matrix
+from scipy.sparse import csr_matrix
 import numpy as np
 
 class OneHotEncoding(BatchTransform):
     def func(self,target_df,Y_df):
-        Y = Y_df.get_matrix().squeeze().tolist()
+        Y = Y_df.get_matrix().squeeze()
+        print Y.shape
+        n=Y.shape[0]
         d = {}
         for y in Y:
             if y not in d:
                 d[y] = len(d)
-        for i,y in enumerate(sorted(d.keys())):
-            d[y] = i
-        OHE = np.zeros((len(Y),len(d)),dtype=bool)
-        for i,y in enumerate(Y):
-            OHE[i,d[y]] = True
-        
+        # for i,y in enumerate(sorted(d.keys())):
+        #     d[y] = i
+        # OHE = np.zeros((len(Y),len(d)),dtype=bool)
+        # for i,y in enumerate(Y):
+        #     OHE[i,d[y]] = True
+        OHE = csr_matrix((np.ones(n),(np.arange(n),Y)),
+                         shape=(n,len(d)),
+                         dtype=bool)
         row_labels =Y_df._row_index.keys()
         col_labels = [str(i) for i in range(len(d))]
 
-        print OHE.shape
-        target_df.set_matrix(OHE,row_labels,col_labels)
+        # print OHE.shape
+        target_df.set_matrix(np.asarray(OHE.todense()),row_labels,col_labels)
