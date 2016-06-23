ff32a8c2cbaa2427e1e37eb7dc9ce504bb65b896
Eric Wong <riceric22@gmail.com>
2016-06-23 17:44:27 -0400

    rewrote pca using the matrix attribute instead of get_matrix

--- dreaml/transformations/pca.py
+++ dreaml/transformations/pca.py
@@ -8,18 +8,16 @@ _auto_dir = "auto/"
 class PCA(BatchTransform):
     def func(self,target_df,X_pca_df,X_full_df,num_bases=50):
         """ Project onto the PCA basis """
-        # PCA function needs a df to store eigen basis in
-        df = target_df._top_df
-        X_pca = X_pca_df.get_matrix(readonly=True)
-        X_mean = np.mean(X_pca,axis=0)
-        # pca_basis_location = (_auto_dir+"features/",_auto_dir+"pca_basis/")
-        # df[pca_basis_location] = PCABasis(X_pca_df,num_bases)
-        # v = df[pca_basis_location].get_matrix()
-        self.v = self.pca_basis(X_pca,num_bases)
-        X_full = X_full_df.get_matrix(readonly=True)
+        X_mean = np.mean(X_pca_df.r_matrix,axis=0)
 
+        # the PCA basis is exposed for the user
+        self.v = self.pca_basis(X_pca_df.r_matrix,num_bases)
+
+        # Use numbers as the label for the basis dimension
         col_labels = [str(i) for i in range(num_bases)]
-        target_df.set_matrix((X_full-X_mean).dot(self.v),
+
+        # Set the matrix with the corresponding labels
+        target_df.set_matrix((X_full_df.r_matrix-X_mean).dot(self.v),
                              row_labels=X_full_df.rows(),
                              col_labels=col_labels)
 
